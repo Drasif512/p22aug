e7a1c2d5b391db58b88d174018c8adcebe3d09ad
muhammadasif.jrw@gmail.com <muhammadasif.jrw@gmail.com>
2023-08-22 09:43:35 +0500

    void practice

---
+++ pvoid.js
@@ -0,0 +1,14 @@
+// function say(): void { 
+//     console.log('friend')
+// } 
+// let sp: void = say(); 
+// console.log(sp); 
+// let nothing: void = undefined;
+// let num: void ;
+// console.log(nothing)
+// console.log(num)
+function as(x) {
+    var y = Math.pow(x, 5);
+    console.log("Dr. asif ", y);
+}
+as(5);
---
+++ pvoid.ts
@@ -0,0 +1,19 @@
+// function say(): void { 
+//     console.log('friend')
+// } 
+// let sp: void = say(); 
+// console.log(sp); 
+
+
+
+// let nothing: void = undefined;
+// let num: void ;
+// console.log(nothing)
+// console.log(num)
+
+function as(x:number) {
+    var y:number=x**5;
+    console.log("Dr. asif ",y)
+    
+}
+as(5);
